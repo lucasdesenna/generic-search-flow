2984f42931b1030093f4329b51fc202063001eda
Lucas de Senna <lucasdesenna@gmail.com>
2016-04-11 23:56:54 -0300

    FIX - Removed unused dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,4 +1,4 @@
-/* global malarkey:false, moment:false */
+/* global moment:false */
 
 import { config } from './index.config';
 import { routerConfig } from './index.route';
@@ -8,8 +8,7 @@ import { MainController } from './main/main.controller';
 import { DetailsController } from './details/details.controller';
 import { RetractableSearchbarDirective } from '../app/components/retractableSearchbar/retractableSearchbar.directive';
 
-angular.module('generic-search-flow', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'infinite-scroll'])
-  .constant('malarkey', malarkey)
+angular.module('generic-search-flow', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr'])
   .constant('moment', moment)
   .config(config)
   .config(routerConfig)

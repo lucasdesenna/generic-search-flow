a03e197475d5aa759e1266f4c58adb7b443438d5
Lucas de Senna <lucasdesenna@gmail.com>
2016-04-12 03:47:03 -0300

    gh-pages updated

--- src/app/index.theme.js
+++
@@ -1,7 +0,0 @@
-export function themeConfig ($mdThemingProvider) {
-  'ngInject';
-
-  $mdThemingProvider.theme('default')
-    .primaryPalette('amber')
-    .accentPalette('deep-orange');
-}
--- src/app/main/main.controller.spec.js
+++
@@ -1,9 +0,0 @@
-describe('controllers', () => {
-  let vm;
-
-  beforeEach(angular.mock.module('generic-search-flow'));
-
-  beforeEach(inject(($controller) => {
-    vm = $controller('MainController');
-  }));
-});
